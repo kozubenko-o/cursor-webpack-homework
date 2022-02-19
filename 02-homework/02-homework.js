45e874d7f4cec61780e2af1c79f15581c8c17159
Oleksandr <alenuryk@gmail.com>
2022-02-19 12:40:48 +0200

    corrected (0 <= 0) is false to true

--- 02-homework/02-homework.js
+++ 02-homework/02-homework.js
@@ -8,7 +8,7 @@ function getNumberFromPrompt(message) {
     do {
         str = prompt(message);
     } while (!str || str.trim() === '' || !Number.isInteger(Number(str)));
-    if (numberN && Number(str) <= numberN) {
+    if (typeof numberN === 'number' && Number(str) <= numberN) {
         alert('Число М повинно бути більшим за число N, N = ' + numberN);
         return getNumberFromPrompt(message);
     } else {
@@ -30,6 +30,6 @@ for (let i = numberN; i <= numberM; i++) {
 document.writeln(`Було введено: <ul> 
                                     <li>число N = ${numberN}</li>
                                     <li>число M = ${numberM}</li>
-                                    <li>Пропускати парні числа - ${missEvenNumbers}</li>
+                                    <li>пропускати парні числа - ${missEvenNumbers}</li>
                                 </ul>
                  Сума до виводу = ${sum}`);
