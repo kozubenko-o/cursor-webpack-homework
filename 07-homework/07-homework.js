8fc4c8cd574b21e36e2e9bebede8edd069ccc6ec
Oleksandr <alenuryk@gmail.com>
2022-06-07 21:28:49 +0300

    homework #7 last function to do

--- 07-homework/07-homework.js
+++ 07-homework/07-homework.js
@@ -61,24 +61,57 @@ function getDividedByFive(...numbers) {
     return numbers.filter(el => el % 5 === 0);
 }
 
-function replaceBadWords(string = 'Holy shit! Are you fucking kidding?') {
+function replaceBadWords(string = 'Holy shit! Are you fucking kidding? Fuck you!') {
     const badWords = ['shit', 'fuck'];
     let newString = string.split(' ');
-    console.log(newString);
-    newString = newString.map((el, index) => {
-        let badIndex = badWords.map(word => {
-            let w = el.split(word);
-            return w.reduce((res, el1) => el1 === '' ? res + index : res, -1);
-        });
-        console.log(badIndex);
-        return badIndex !== '' ? index : -1;
+    newString = newString.map(word => {
+        let badWord = badWords.find(bw => word.toLowerCase().includes(bw.toLowerCase()));
+        let newWord = word;
+        if (badWord) {
+            if (badWord.toLowerCase() === word.toLowerCase()) {
+                newWord = '****';
+            } else {
+                newWord = word.toLowerCase().split(badWord.toLowerCase())
+                    .reduce((res, p) => p === '' ? res + '****' : res + p, '');
+            }
+        }
+        return newWord;
     });
-
-    console.log(newString);
     return newString.join(' ');
 }
+
+function divideByThree(string = 'Live Commander') {
+    return  string.toLowerCase().split(' ').join('').split('')
+                .reduce((res, el) =>
+                    (res.length + 1) % 4 === 0 && res.length!== 0 ? res + ' ' + el : res + el , '')
+                .split(' ');
+}
+
+function generateCombinations(word) {
+    if (word.length > 10) {
+        return '!!!К-сть букв у слові більше за 10';
+    }
+    if (word.length <= 1) {
+        return word;
+    }
+    let result = word.toLowerCase().split('');
+    let factorial = result.reduce((res, el, index) => res * (index + 1), 1);
+    result = Array.from(Array(Math.floor(factorial)), () => {
+        result.map((el, index) => {
+            result.reduce((res, el1,index2) => {
+                if (index === index2) {
+                    return res;
+                } else {
+
+                }
+            }, '')
+        })
+    });
+    return factorial;
+}
 console.log('Масив рандомных чисел:  ', randomArray);
-console.log(`Мода масиву ${randomArray}:   ${getModa(randomArray)}`);
+console.log(`Мода масиву ${randomArray}:`);
+console.log(getModa(randomArray));
 //console.log(`Мода масиву 2,3.5,2,3.5,3.5,2.3,2.1,3.5:   ${getModa(2,3.5,2,3.5,3.5,2.3,2.1,3.5)}`);
 console.log(`Середнє арифметичне масиву ${randomArray}:   ${getAverage(randomArray).toFixed(2)}`);
 //console.log(`Середнє арифметичне масиву 2,2,2,2,2,2.3,2.1,3.5:   ${getAverage(2,2,2,2,2,2.3,2.1,3.5)}`);
@@ -89,4 +122,13 @@ console.log(`Фільтрація парних чисел масиву ${randomA
 console.log(`К-сть позитивних єлементів масиву ${randomArray}:   ${countPositiveNumbers(randomArray)}`);
 //console.log(`К-сть позитивних єлементів масиву -2,-1,-15,-2.5,2,-3.-2,2.-2,2,12:   ${countPositiveNumbers(-2,-1,-15,-2.5,2,-3.-2,2.-2,2,12)}`);
 console.log(`${randomArray} // Діляться на ціло на 5:   ${getDividedByFive(randomArray)}`);
-console.log(replaceBadWords());
+console.log('Заміняємо погані слова: ' + replaceBadWords());
+console.log('Розбиваємо кожне слово на умовні склади по 3 букви: ' + divideByThree());
+console.log(generateCombinations('coffee'));
+
+1 2 3 4
+1 2 4 3
+1 4 3 2 
+4 2 3 1
+1 3 2 4
+3 2 1 4
