818ee3d039309683312cdcb71d253c20d794c82c
Oleksandr <alenuryk@gmail.com>
2022-06-14 19:51:58 +0300

    homework #9 added

--- 09-homework/09-homework.js
+++ 09-homework/09-homework.js
@@ -4,6 +4,30 @@ const latvia = { tax: 0.25, middleSalary: 1586, vacancies: 3921 };
 
 const litva = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };
 
-function getMyTaxes(country, salary) {}
+function getMyTaxes(salary) {
+    return this.tax * salary;
+}
 
-getMyTaxes.call(country, salary);
+function getMiddleTaxes() {
+    return this.tax * this.middleSalary;
+}
+
+function getTotalTaxes() {
+    return this.tax * this.middleSalary * this.vacancies;
+}
+
+function getMySalary() {
+    const salary = Math.floor(Math.random()*501 + 1500);
+    const taxes = getMyTaxes.call(this, salary);
+    console.log({
+        salary: salary,
+        taxes: +taxes.toFixed(2),
+        profit: +(salary - taxes).toFixed(2)
+    });
+    setTimeout(() => getMySalary.call(this), 10000);
+}
+
+console.log(getMyTaxes.call(ukraine, 1000));
+console.log(getMiddleTaxes.call(latvia));
+console.log(getTotalTaxes.call(litva));
+getMySalary.call(ukraine);
