bef96329946244ce5ee997b18154455ce769b249
Oleksandr <alenuryk@gmail.com>
2022-07-19 21:49:38 +0300

    added HW #17. attempt #3

--- 17-homework/webpack.config.js
+++ 17-homework/webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
     entry: path.resolve(__dirname, "src/index.js"),
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js"
+        filename: "bundle.js",
+        publicPath: '/assets'
     },
     devServer: {
         port: 4200,
